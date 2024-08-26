bfa42a2aedeefcac65bd7ebee7a36ff19a13246d
ductd10062003 <ductdhe176150@fpt.edu.vn>
2024-08-26 23:33:04 +0700

    update

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { UserUtility } from '../../utilities/user-utility';
 import { Router } from '@angular/router';
 
@@ -14,10 +14,25 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
   applyForm = new FormGroup({
-    username: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl('')
-  })
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    repassword: new FormControl('',[Validators.required])
+  }, { validators: this.matchPasswordValidator('password', 'repassword') })
+
+  matchPasswordValidator(controlName: string, matchingControlName: string): ValidatorFn {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const control = formGroup.get(controlName);
+      const matchingControl = formGroup.get(matchingControlName);
+  
+      if (control && matchingControl && control.value !== matchingControl.value) {
+        return { 'mismatch': true };
+      }
+  
+      return null;
+    };
+  }
+
   constructor(private userService: UserService, private router: Router){}
 
   submitForm(){
@@ -26,6 +41,9 @@ export class RegisterComponent {
     this.userService.register(user).subscribe(res => {
       if(res.success === 'true'){
         this.router.navigate(['/login']);
+      }
+      else{
+        alert('Error')
       }
     })
   }
